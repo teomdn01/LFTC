3adc01da36a41a432da9cd7fd2b4d1c077bc32a8
teo <teo@DESKTOP-08UKAEE>
2022-12-11 17:56:20 +0200

    add grammar 2

---
+++ mini-language/Grammar.java
@@ -0,0 +1,84 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Grammar {
+    private static String TERMINAL_AND_NON_TERMINAL_SEPARATOR = "@";
+    private static String LEFT_HAND_SIDE_TO_RIGHT_SEPARATOR = "->";
+    private static String TRANSITION_OR_SEPARATOR = "\\|";
+    private static String TRANSITION_CONCATENATION = " ";
+
+
+    private Set<String> nonTerminals;
+    private Set<String> terminals;
+    private String startingSymbol;
+    private HashMap<List<String>, List<List<String>>> productions = new HashMap<>();
+
+    public Grammar(String filename) {
+        this.loadFromFile(filename);
+    }
+
+    public Set<String> getNonTerminals() {
+        return nonTerminals;
+    }
+
+    public Set<String> getTerminals() {
+        return terminals;
+    }
+
+    public String getStartingSymbol() {
+        return startingSymbol;
+    }
+
+    public HashMap<List<String>, List<List<String>>> getProductions() {
+        return productions;
+    }
+
+    private void loadFromFile(String filename) {
+        try (Scanner scanner = new Scanner(new File(filename))){
+            nonTerminals = new HashSet<>(List.of(scanner.nextLine().split(TERMINAL_AND_NON_TERMINAL_SEPARATOR)));
+            terminals = new HashSet<>(List.of(scanner.nextLine().split(TERMINAL_AND_NON_TERMINAL_SEPARATOR)));
+            startingSymbol = scanner.nextLine();
+
+
+            while (scanner.hasNextLine()) {
+                processProduction(scanner.nextLine());
+
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void processProduction(String production) {
+        String[] line = production.split(LEFT_HAND_SIDE_TO_RIGHT_SEPARATOR);
+        List<String> leftHandSide = List.of(line[0].split(TRANSITION_CONCATENATION));
+        this.productions.putIfAbsent(leftHandSide, new ArrayList<>());
+        String rightHandSide = line[1];
+        String[] rightSplit = rightHandSide.split(TRANSITION_OR_SEPARATOR);
+        Arrays.stream(rightSplit).forEach(rs -> this.productions.get(leftHandSide)
+                .add(Arrays.stream(rs.split(TRANSITION_CONCATENATION))
+                .collect(Collectors.toList())));
+    }
+
+    public List<List<String>> getProductionsForNonTerminal(List<String> nonTerminal) {
+        return this.productions.get(nonTerminal);
+    }
+
+    public boolean performCFGCheck() {
+        if (!nonTerminals.contains(startingSymbol)) {
+            return false;
+        }
+
+        for (List<String> key : productions.keySet()) {
+            if (key.size() != 1 || !nonTerminals.contains(key.get(0))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
--- mini-language/Main.java
+++ mini-language/Main.java
@@ -1,16 +1,26 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        FiniteAutomaton faLab = new FiniteAutomaton("IO/FA.in");
+       // FiniteAutomaton faLab = new FiniteAutomaton("IO/FA.in");
+        Grammar grammar = new Grammar("IO/G2.txt");
 
         String baseFilename = "p3";
         FiniteAutomaton identifierFA = new FiniteAutomaton("IO/FAIdentifier.in");
         FiniteAutomaton constantFA = new FiniteAutomaton("IO/FAConstant.in");
         MiniScanner scanner = new MiniScanner("IO/" + baseFilename + ".txt", identifierFA, constantFA);
         while (true) {
-            System.out.println("Select option: 1 -> view FAs, 2 -> run scanner");
+            System.out.println("Select option: ");
+            System.out.println("1 -> View FAs ");
+            System.out.println("2 -> Run scanner");
+            System.out.println("3 -> Print grammar set of non terminals");
+            System.out.println("4 -> Print grammar set of terminals");
+            System.out.println("5 -> Print grammar set of productions");
+            System.out.println("6 -> Print grammar productions for a given non terminal");
+            System.out.println("7 -> Perform grammar CFG check");
+
             Scanner scannerIO = new Scanner(System.in);
             int option = scannerIO.nextInt();
             if (option == 1) {
@@ -22,6 +32,24 @@ public class Main {
                 scanner.printSymbolTable("IO/" + baseFilename + "ST.txt");
                 scanner.printPIF("IO/" + baseFilename + "PIF.txt");;
                 System.out.println("Done");
+            }
+            else if (option == 3) {
+                System.out.println(grammar.getNonTerminals());
+            }
+            else if (option == 4) {
+                System.out.println(grammar.getTerminals());
+            }
+            else if (option == 5) {
+                System.out.println(grammar.getProductions());
+            }
+            else if (option == 6) {
+                scannerIO.nextLine();
+                System.out.println("Choose non terminal: " + grammar.getNonTerminals());
+                String nonTerminal = scannerIO.nextLine();
+                System.out.println(grammar.getProductionsForNonTerminal(List.of(nonTerminal)));
+            }
+            else if (option == 7) {
+                System.out.println(grammar.performCFGCheck());
             }
         }
 
